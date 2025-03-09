ec44b4a5867ed1c132924d1cce3666de45366bea
Rhyker Wells <a.rhykerw@gmail.com>
2025-03-09 11:34:06 +0000

    feat: Switch log to logrus

--- internal/run/run.go
+++ internal/run/run.go
@@ -1,36 +1,35 @@
 package run
 
 import (
-	"fmt"
 	"os"
 	"os/signal"
-	"log"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	log "github.com/sirupsen/logrus"
 )
 
 func Run() {
 	discord, err := discordgo.New("Bot " + os.Getenv("ASBWIG_TOKEN"))
 	if err != nil {
-		log.Fatal("ERROR LOGGING IN\n", err)
+		log.Fatalln("ERROR LOGGING IN\n", err)
 	}
 
 	err = discord.Open()
 	if err != nil {
-		log.Fatal("ERROR OPENING CONNECTION\n", err)
+		log.Fatalln("ERROR OPENING CONNECTION\n", err)
 	}
 	defer discord.Close()
 	closeHandler(discord)
 }
 
 func closeHandler(discord *discordgo.Session) {
-	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	log.Infoln("Bot is now running. Press CTRL-C to exit.")
     sc := make(chan os.Signal, 1)
     signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
     <-sc
 
     // Cleanly close down the Discord session.
-	fmt.Println("Exiting now....")
+	log.Infoln("Exiting now....")
     discord.Close()
 }
