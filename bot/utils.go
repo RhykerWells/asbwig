3849992f8b2757d3670fdf190557e6d8c2203f6c
Rhyker Wells <a.rhykerw@gmail.com>
2025-03-14 10:36:37 +0000

    feat(utils): Add basic message functions

---
+++ bot/utils.go
@@ -0,0 +1,39 @@
+package bot
+
+import (
+	"time"
+
+	"github.com/Ranger-4297/asbwig/internal"
+	"github.com/bwmarrin/discordgo"
+	"github.com/sirupsen/logrus"
+)
+
+// Message functions
+func SendMessage(c string, messageData *discordgo.MessageSend) (messageObject *discordgo.Message, err error) {
+	messageObject, err = internal.Session.ChannelMessageSendComplex(c, messageData)
+	return
+}
+
+func SendDM(user string, msg *discordgo.MessageSend) error {
+	channel, err := internal.Session.UserChannelCreate(user)
+	if err != nil {
+		return err
+	}
+
+	_, err = SendMessage(channel.ID, msg)
+	return err
+}
+
+func SendMessageToDeleteAfter(delay time.Duration, c string, messageData *discordgo.MessageSend) {
+	message, err := SendMessage(c, messageData)
+	if err != nil {
+		logrus.Warn("Failed to send temporary message")
+	}
+	DeleteChannelMessageAfterDelay(delay, c, message.ID)
+}
+
+func DeleteChannelMessageAfterDelay(delay time.Duration, c string, m string) error {
+    time.Sleep(delay)
+    err := internal.Session.ChannelMessageDelete(c, m)
+    return err
+}
