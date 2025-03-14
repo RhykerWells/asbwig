bbf9c7a9179c3407a116efe44985f9f3acc1371d
Rhyker Wells <a.rhykerw@gmail.com>
2025-03-14 14:12:02 +0000

    feat!(bot): Update utils_test logging

--- bot/utils_test.go
+++ bot/utils_test.go
@@ -1,19 +1,41 @@
 package bot
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
 
 func TestToInt64(t *testing.T) {
 	// String test
 	i := ToInt64(1)
-	t.Logf("integer test: %d || SHOULD WORK", i)
+	logrus.WithFields(logrus.Fields{
+		"Input": "int: 1",
+		"Output": i,
+		}).Info()
 	i = ToInt64("2")
-	t.Logf("String test: %d || SHOULD WORK", i)
+	logrus.WithFields(logrus.Fields{
+		"Input": `String: 2`,
+		"Output": i,
+		}).Info()
 	i = ToInt64(3.0)
-	t.Logf("flat float test: %d || SHOULD WORK", i)
+	logrus.WithFields(logrus.Fields{
+		"Input": "Float: 3.0",
+		"Output": i,
+		}).Info()
 	i = ToInt64("4.0")
-	t.Logf("string flat float test: %d || SHOULD WORK", i)
+	logrus.WithFields(logrus.Fields{
+		"Input": `Float (string): 4.0`,
+		"Output": i,
+		}).Info()	
 	i = ToInt64(5.1)
-	t.Logf("non flat float test: %d", i)
+	logrus.WithFields(logrus.Fields{
+		"Input": "Float: 5.1",
+		"Output": i,
+		}).Info()
 	i = ToInt64("6.1")
-	t.Logf("string non flat float test: %d", i)
+	logrus.WithFields(logrus.Fields{
+		"Input": `Float: (string) 6.1`,
+		"Output": i,
+		}).Info()
 }
