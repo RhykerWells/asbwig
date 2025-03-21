427228cfbc0f072ed75e8fdb977b65051bd1234a
Rhyker Wells <a.rhykerw@gmail.com>
2025-03-21 15:20:18 +0000

    feat(ping): Update command to show edit latency

--- commands/ping/ping.go
+++ commands/ping/ping.go
@@ -1,18 +1,21 @@
 package ping
 
 import (
-	"github.com/Ranger-4297/asbwig/common/dcommand"
+	"time"
+
 	"github.com/Ranger-4297/asbwig/bot/functions"
-	"github.com/bwmarrin/discordgo"
+	"github.com/Ranger-4297/asbwig/common/dcommand"
 )
 
 var Command = &dcommand.AsbwigCommand {
 	Command:	[]string{"ping"},
 	Description: "Displays bot latency",
 	Run: (func(data *dcommand.Data) {
-		message := &discordgo.MessageSend {
-			Content: "Pong",
-		}
-		functions.SendMessage(data.Message.ChannelID, message)
+	msg, err  := functions.SendBasicMessage(data.Message.ChannelID, "Ping...")
+	if err != nil {
+		return
+	}
+	started := time.Now()
+	functions.EditBasicMessage(msg.ChannelID, msg.ID, "Pong! (Edit): "+ (time.Since(started) * time.Microsecond).String())
 	}),
 }
