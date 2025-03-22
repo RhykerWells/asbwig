909c529980c546f0eb7708f81e080331a29163a6
Rhyker Wells <a.rhykerw@gmail.com>
2025-03-22 17:41:20 +0000

    feat(commands/setstatus): Add arg handle for help

--- commands/botOwner/setstatus/setstatus.go
+++ commands/botOwner/setstatus/setstatus.go
@@ -13,6 +13,9 @@ var Command = &dcommand.AsbwigCommand {
 	Command:		[]string{"setstatus"},
 	Description: 	"Changes the bot status",
 	ArgsRequired:	1,
+	Args:			[]*dcommand.Args{
+		{Name:	"Status", Type:	dcommand.String},
+	},
 	Run: util.OwnerCommand(func(data *dcommand.Data) {
 		functions.SetStatus(strings.Join(data.Args, " "))
 		message := &discordgo.MessageSend {
