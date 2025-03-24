f567ceaa0f64a33e7d92ff2775fa329f18fe67c0
Rhyker Wells <a.rhykerw@gmail.com>
2025-03-24 20:17:43 +0000

    feat(economy): Add database query and selectors

--- commands/economy/economy.go
+++ commands/economy/economy.go
@@ -1,10 +1,45 @@
 package economy
 
-const GuildEconomySchema = `
-	CREATE TABLE IF NOT EXISTS economy_config (
-		guild_id BIGINT PRIMARY KEY,
-		max_bet BIGINT NOT NULL DEFAULT '5000',
-		symbol TEXT NOT NULL DEFAULT '£',
-		start_balance BIGINT NOT NULL DEFAULT '200'
-	)
-`
+//go:generate sqlboiler --no-hooks psql
+
+import (
+	"github.com/RhykerWells/asbwig/common"
+	"github.com/RhykerWells/asbwig/common/dcommand"
+)
+
+var GuildEconomySchema = []string{`
+CREATE TABLE IF NOT EXISTS economy_config (
+	guild_id BIGINT PRIMARY KEY,
+	max_bet BIGINT NOT NULL DEFAULT '5000',
+	symbol TEXT NOT NULL DEFAULT '£',
+	start_balance BIGINT NOT NULL DEFAULT '200'
+);
+`,`
+CREATE TABLE IF NOT EXISTS economy_cash (
+	guild_id BIGINT PRIMARY KEY,
+	user_id BIGINT NOT NULL,
+	cash BIGINT
+)
+`}
+func EconomySetup(cmdHandler *dcommand.CommandHandler) {
+	common.InitSchema("Economy", GuildEconomySchema...)
+}
+
+func GuildEconomyAdd(guild_id string) {
+	const query = `
+	SELECT guild_id FROM economy_config
+	WHERE guild_id=$1
+	`
+	err := common.PQ.QueryRow(query, guild_id)
+	if err != nil {
+		guildEconomyDefault(guild_id)
+	}
+}
+
+func guildEconomyDefault(guild_id string) {
+	const query = `
+	INSERT INTO economy_config (guild_id)
+	VALUES ($1)
+	`
+	common.PQ.Exec(query, guild_id)
+}
