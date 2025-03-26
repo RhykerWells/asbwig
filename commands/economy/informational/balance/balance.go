44970b2fe98a065705af88bf053072433b3dc792
Rhyker Wells <a.rhykerw@gmail.com>
2025-03-26 18:57:41 +0000

    feat(balance): Add bank & networth to balance output

--- commands/economy/informational/balance/balance.go
+++ commands/economy/informational/balance/balance.go
@@ -14,12 +14,19 @@ var Command = &dcommand.AsbwigCommand{
 	Command:     []string{"balance"},
 	Description: "Views your balance in the economy",
 	Run: (func(data *dcommand.Data) {
-		const query = `SELECT cash FROM economy_cash WHERE guild_id=$1 AND user_id=$2`
-		var cash string
-		err := common.PQ.QueryRow(query, data.Message.GuildID, data.Message.Author.ID).Scan(&cash)
+		const cashquery = `SELECT cash FROM economy_cash WHERE guild_id=$1 AND user_id=$2`
+		var cash int64
+		err := common.PQ.QueryRow(cashquery, data.Message.GuildID, data.Message.Author.ID).Scan(&cash)
 		if err != nil {
-			cash = "0"
+			cash = 0
 		}
+		const bankquery = `SELECT balance FROM economy_bank WHERE guild_id=$1 AND user_id=$2`
+		var bank int64
+		err = common.PQ.QueryRow(bankquery, data.Message.GuildID, data.Message.Author.ID).Scan(&bank)
+		if err != nil {
+			bank = 0
+		}
+		networth := cash + bank
 		embed := &discordgo.MessageEmbed {
 			Author: &discordgo.MessageEmbedAuthor{
 				Name:    data.Message.Author.Username,
@@ -28,8 +35,19 @@ var Command = &dcommand.AsbwigCommand{
 			Description: fmt.Sprintf("%s's balance", data.Message.Author.Mention()),
 			Fields: []*discordgo.MessageEmbedField {
 				{
-					Name: "cash",
-					Value: cash,
+					Name: "Cash",
+					Value: fmt.Sprint(cash),
+					Inline: true,
+				},
+				{
+					Name: "Bank",
+					Value: fmt.Sprint(bank),
+					Inline: true,
+				},
+				{
+					Name: "Networth",
+					Value: fmt.Sprint(networth),
+					Inline: true,
 				},
 			},
 			Timestamp: time.Now().Format(time.RFC3339),
