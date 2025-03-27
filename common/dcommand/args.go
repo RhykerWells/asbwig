3dedb80c3f7a137ffb052cf47c4db63c25cfc904
Rhyker Wells <a.rhykerw@gmail.com>
2025-03-27 13:19:34 +0000

    feat(dcommand): Add int argument support

--- common/dcommand/args.go
+++ common/dcommand/args.go
@@ -10,11 +10,18 @@ type ArgumentType interface {
 }
 
 var (
-	String = &StringArg{}
+	String =	&StringArg{}
+	Int =		&IntArg{}
 )
 
 type StringArg struct{}
 var _ ArgumentType = (*StringArg)(nil)
 func (s *StringArg) Help() string {
 	return "Text"
+}
+
+type IntArg struct{}
+var _ ArgumentType = (*IntArg)(nil)
+func (s *IntArg) Help() string {
+	return "Integer"
 }
