36bde061b12270a24d03ab58c7b4eea4e26787a1
Rhyker Wells <a.rhykerw@gmail.com>
2025-03-27 15:01:42 +0000

    feat(commands): Adds Admin/ManageServer permission check

--- commands/util/util.go
+++ commands/util/util.go
@@ -1,14 +1,29 @@
 package util
 
 import (
+	"github.com/RhykerWells/asbwig/bot/functions"
 	"github.com/RhykerWells/asbwig/common"
 	"github.com/RhykerWells/asbwig/common/dcommand"
+	"github.com/bwmarrin/discordgo"
 )
 
 func OwnerCommand(inner dcommand.Run) dcommand.Run {
 	return func(data *dcommand.Data) {
 		if data.Message.Author.ID == common.ConfigBotOwner {
 			inner(data)
+		} else {
+			functions.SendBasicMessage(data.Message.ChannelID, "This is a bot-owner only command.")
+		}
+	}
+}
+
+func AdminOrManageServerCommand(inner dcommand.Run) dcommand.Run {
+	return func(data *dcommand.Data) {
+		perms, _ := data.Session.State.UserChannelPermissions(data.Message.Author.ID, data.Message.ChannelID)
+		if perms&discordgo.PermissionAdministrator == 8 || perms&discordgo.PermissionManageServer == 32 {
+			inner(data)
+		} else {
+			functions.SendBasicMessage(data.Message.ChannelID, "You need `Administrator` or `ManageServer` permissions to use this command.")
 		}
 	}
 }
