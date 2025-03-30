8fe44fe70430707a4e2b7791646022127b4aaf3f
Rhyker Wells <a.rhykerw@gmail.com>
2025-03-30 23:03:46 +0000

    feat(economy): Snakeeyes command with `<Bet>*36` payout

--- commands/economy/economy.go
+++ commands/economy/economy.go
@@ -8,6 +8,7 @@ import (
 	"github.com/RhykerWells/asbwig/commands/economy/moneyMaking/crime"
 	"github.com/RhykerWells/asbwig/commands/economy/moneyMaking/rob"
 	"github.com/RhykerWells/asbwig/commands/economy/moneyMaking/rollNumber"
+	"github.com/RhykerWells/asbwig/commands/economy/moneyMaking/snakeEyes"
 	"github.com/RhykerWells/asbwig/commands/economy/moneyMaking/work"
 	"github.com/RhykerWells/asbwig/commands/economy/moneyManagement/addMoney"
 	"github.com/RhykerWells/asbwig/commands/economy/moneyManagement/deposit"
@@ -29,6 +30,7 @@ func EconomySetup(cmdHandler *dcommand.CommandHandler) {
 		crime.Command,
 		rob.Command,
 		rollnumber.Command,
+		snakeeyes.Command,
 		addmoney.Command,
 		removemoney.Command,
 		deposit.Command,
---
+++ commands/economy/moneyMaking/snakeEyes/snakeEyes.go
@@ -0,0 +1,103 @@
+package snakeeyes
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/RhykerWells/asbwig/bot/functions"
+	"github.com/RhykerWells/asbwig/commands/economy/models"
+	"github.com/RhykerWells/asbwig/common"
+	"github.com/RhykerWells/asbwig/common/dcommand"
+	"github.com/bwmarrin/discordgo"
+	"github.com/dustin/go-humanize"
+	"github.com/volatiletech/null/v8"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+)
+
+var Command = &dcommand.AsbwigCommand{
+	Command:     "snakeeyes",
+	Aliases: 	 []string{""},
+	Description: "Rolls 2 6-sided dice, with a payout of `<Bet>*36` if they both land on 1",
+	Args: []*dcommand.Args{
+		{Name: "Bet", Type: dcommand.Int},
+	},
+	Run: func(data *dcommand.Data) {
+		embed := &discordgo.MessageEmbed{Author: &discordgo.MessageEmbedAuthor{Name: data.Author.Username, IconURL: data.Author.AvatarURL("256")}, Timestamp: time.Now().Format(time.RFC3339), Color: common.ErrorRed}
+		userRoll, err := models.EconomyCooldowns(qm.Where("guild_id=? AND user_id=? AND type = 'snakeeyes' WHERE expires_at > NOW()", data.GuildID, data.Author.ID)).One(context.Background(), common.PQ)
+		if err == nil {
+			if userRoll.ExpiresAt.Time.After(time.Now()) {
+				embed.Description = "This command is on cooldown"
+				functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
+				return
+			}
+		}
+		guild, _ := models.EconomyConfigs(qm.Where("guild_id=?", data.GuildID)).One(context.Background(), common.PQ)
+		userCash, err := models.EconomyCashes(qm.Where("guild_id=? AND user_id=?", data.GuildID, data.Author.ID)).One(context.Background(), common.PQ)
+		var cash int64 = 0
+		if err == nil {
+			cash = userCash.Cash
+		}
+		if len(data.Args) <= 0 {
+			embed.Description = "No `Bet` argument provided"
+			functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
+			return
+		}
+		amount := data.Args[0]
+		if functions.ToInt64(amount) <= 0 && amount != "all" && amount != "max" {
+			embed.Description = "Invalid `Bet` argument provided"
+			functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
+			return
+		}
+		bet := int64(0)
+		if amount == "all" {
+			bet = cash
+		} else if amount == "max" {
+			if guild.Maxbet > 0 {
+				bet = guild.Maxbet
+			} else {
+				bet = cash
+			}
+		} else {
+			bet = functions.ToInt64(amount)
+		}
+		if bet > cash {
+			embed.Description = fmt.Sprintf("You can't bet more than you have in your hand. You currently have %s%s", guild.Symbol, humanize.Comma(cash))
+			functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
+			return
+		}
+		if guild.Maxbet > 0 && bet > guild.Maxbet {
+			embed.Description = fmt.Sprintf("You can't bet more than the servers limit. The limit is %s%s", guild.Symbol, humanize.Comma(guild.Maxbet))
+			functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
+			return
+		}
+		d1, d2 := rand.Int63n(6)+1, rand.Int63n(6)+1
+		condition := "won"
+		if d1 == 1 && d2 == 1 {
+			cash = cash + (bet * 36)
+		} else {
+			cash = cash - bet
+			condition = "lost"
+		}
+		embed.Description = fmt.Sprintf("You rolled %d & %d, and you %s %s%s", d1, d2, condition, guild.Symbol, humanize.Comma(bet))
+		functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
+		cashEntry := models.EconomyCash{
+			GuildID: data.GuildID,
+			UserID: data.Author.ID,
+			Cash: cash,
+		}
+		_ = cashEntry.Upsert(context.Background(), common.PQ, true, []string{"guild_id", "user_id"}, boil.Whitelist("cash"), boil.Infer())
+		cooldowns := models.EconomyCooldown{
+			GuildID: data.GuildID,
+			UserID:  data.Author.ID,
+			Type:    "snakeeyes",
+			ExpiresAt: null.Time{
+				Time:  time.Now().Add(300 * time.Second),
+				Valid: true,
+			},
+		}
+		cooldowns.Upsert(context.Background(), common.PQ, true, []string{"guild_id", "user_id", "type"}, boil.Whitelist("expires_at"), boil.Infer())
+	},
+}
