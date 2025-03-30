1a9030f266d1fa85d226b545f1d07442626fdc5e
Rhyker Wells <a.rhykerw@gmail.com>
2025-03-30 15:20:09 +0000

    feat(economy): Update schema to handle constraints

--- commands/economy/schema.go
+++ commands/economy/schema.go
@@ -17,12 +17,30 @@ CREATE TABLE IF NOT EXISTS economy_cash (
 	cash BIGINT NOT NULL
 );
 `,`
+DO $$
+BEGIN
+  BEGIN
+    ALTER TABLE economy_cash ADD CONSTRAINT economy_cash_unique UNIQUE (guild_id, user_id);
+  EXCEPTION
+    WHEN duplicate_table THEN RAISE NOTICE 'Table constraint economy_cash_unique already exists';
+  END;
+END $$;
+`,`
 CREATE TABLE IF NOT EXISTS economy_bank (
 	ID SERIAL PRIMARY KEY,
 	guild_id TEXT NOT NULL,
 	user_id TEXT NOT NULL,
 	balance BIGINT NOT NULL
 );`,`
+DO $$
+BEGIN
+  BEGIN
+    ALTER TABLE economy_bank ADD CONSTRAINT economy_bank_unique UNIQUE (guild_id, user_id);
+  EXCEPTION
+    WHEN duplicate_table THEN RAISE NOTICE 'Table constraint economy_bank_unique already exists';
+  END;
+END $$;
+`,`
 CREATE TABLE IF NOT EXISTS economy_cooldowns (
 	ID SERIAL PRIMARY KEY,
 	guild_id TEXT NOT NULL,
