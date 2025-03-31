7513e2f75cd225828fd47590ed56783838d413c2
Rhyker Wells <a.rhykerw@gmail.com>
2025-03-31 15:31:52 +0000

    feat(bot): Properly upsert and delete entries when members join/leave guilds

--- bot/events/member.go
+++ bot/events/member.go
@@ -14,17 +14,19 @@ func guildMemberLeave(s *discordgo.Session, m *discordgo.GuildMemberRemove) {
 	guildid := m.GuildID
 	userid := m.Member.User.ID
 	_, _ = models.EconomyCashes(qm.Where("guild_id=?", guildid), qm.Where("user_id=?", userid)).DeleteAll(context.Background(), common.PQ)
+	_, _ = models.EconomyBanks(qm.Where("guild_id=?", guildid), qm.Where("user_id=?", userid)).DeleteAll(context.Background(), common.PQ)
+	_, _ = models.EconomyCooldowns(qm.Where("guild_id=?", guildid), qm.Where("user_id=?", userid)).DeleteAll(context.Background(), common.PQ)
 }
 
 func guildMemberAdd(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
 	guildid := m.GuildID
 	userid := m.Member.User.ID
 	guild, _ := models.EconomyConfigs(qm.Where("guild_id=?", guildid)).One(context.Background(), common.PQ)
-	balance := guild.Startbalance
 	
-	var cash models.EconomyCash
-	cash.GuildID = guildid
-	cash.UserID = userid
-	cash.Cash = balance
+	cash := models.EconomyCash{
+		GuildID: guildid,
+		UserID: userid,
+		Cash: guild.Startbalance,
+	}
 	_ = cash.Insert(context.Background(), common.PQ, boil.Infer())
 }
