8717ae70747e9400c5a3b711dcab9dfb149e7f90
Rhyker Wells <a.rhykerw@gmail.com>
2025-04-05 02:53:21 +0000

    style(bot/events): format code with gofmt, add doc comments

--- bot/events/ready.go
+++ bot/events/ready.go
@@ -5,8 +5,9 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// Send message event
+// botReady is sent when the bot originally connects to the gateway
+// This is used to test the bot actually connects
 func botReady(s *discordgo.Session, r *discordgo.Ready) {
-    guildCount := len(r.Guilds)
+	guildCount := len(r.Guilds)
 	log.Infof("Connected to: %d guilds", guildCount)
 }
