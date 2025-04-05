c8e3d4627fe48caab369cd01bfb7f9238737d8bc
Rhyker Wells <a.rhykerw@gmail.com>
2025-04-05 02:52:23 +0000

    refactor(bot/events): format code with gofmt, restructure code for consistency

--- bot/events/member.go
+++ bot/events/member.go
@@ -10,22 +10,27 @@ import (
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
-func guildMemberLeave(s *discordgo.Session, m *discordgo.GuildMemberRemove) {
-	guildid := m.GuildID
-	userid := m.Member.User.ID
-	models.EconomyUsers(qm.Where("guild_id=? AND user_id=?", guildid, userid)).DeleteAll(context.Background(), common.PQ)
-	models.EconomyCooldowns(qm.Where("guild_id=? AND user_id=?", guildid, userid)).DeleteAll(context.Background(), common.PQ)
-}
 
+// guildMemberAdd is called when a member joins a guild the bot is in
+// This adds the user to any tables that are relevant to them
 func guildMemberAdd(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
 	guildid := m.GuildID
 	userid := m.Member.User.ID
 	guild, _ := models.EconomyConfigs(qm.Where("guild_id=?", guildid)).One(context.Background(), common.PQ)
 	userEntry := models.EconomyUser{
 		GuildID: guildid,
-		UserID: userid,
-		Cash: guild.Startbalance,
-		Bank: 0,
+		UserID:  userid,
+		Cash:    guild.Startbalance,
+		Bank:    0,
 	}
 	userEntry.Insert(context.Background(), common.PQ, boil.Infer())
+}
+
+// guildMemberLeave is called when a member leaves a guild the bot is in
+// This removes the user from any tables that they may be part of
+func guildMemberLeave(s *discordgo.Session, m *discordgo.GuildMemberRemove) {
+	guildid := m.GuildID
+	userid := m.Member.User.ID
+	models.EconomyUsers(qm.Where("guild_id=? AND user_id=?", guildid, userid)).DeleteAll(context.Background(), common.PQ)
+	models.EconomyCooldowns(qm.Where("guild_id=? AND user_id=?", guildid, userid)).DeleteAll(context.Background(), common.PQ)
 }
