09a410523be534e991741f90edfd28d2bdebe1e2
Rhyker Wells <a.rhykerw@gmail.com>
2025-04-08 03:44:43 +0000

    feat(commands): Add generic command category help output

--- commands/help.go
+++ commands/help.go
@@ -2,17 +2,20 @@ package commands
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/RhykerWells/asbwig/bot/functions"
 	"github.com/RhykerWells/asbwig/common"
 	"github.com/RhykerWells/asbwig/common/dcommand"
 	"github.com/bwmarrin/discordgo"
+	"github.com/sirupsen/logrus"
 )
 
 var helpCmd = &dcommand.AsbwigCommand{
-	Command: "help",
-	Aliases: []string{"h"},
+	Command:  "help",
+	Aliases:  []string{"h"},
+	Category: dcommand.CategoryGeneral,
 	Args: []*dcommand.Args{
 		{Name: "Command", Type: dcommand.String, Optional: true},
 	},
@@ -33,18 +36,49 @@ func helpFunc(data *dcommand.Data) {
 	}
 
 	// Generic help category
-	basicEmbed := &discordgo.MessageEmbed{
+	genericCategoryHelp(data.ChannelID)
+}
+
+func genericCategoryHelp(channelID string) {
+	cmdMap := dcommand.CmdHndlr.RegisteredCommands()
+	categories := make(map[string][]string)
+	for _, cmd := range cmdMap {
+		categories[cmd.Category.Name] = append(categories[cmd.Category.Name], cmd.Trigger)
+	}
+	categoryNames := make([]string, 0, len(categories))
+	for categoryName := range categories {
+		categoryNames = append(categoryNames, categoryName)
+	}
+	sort.SliceStable(categoryNames, func(i, j int) bool {
+		if categoryNames[i] == "General" {
+			return true
+		}
+		if categoryNames[j] == "General" {
+			return false
+		}
+		return categoryNames[i] < categoryNames[j]
+	})
+	logrus.Warnln(categories)
+
+	helpEmbed := &discordgo.MessageEmbed{
 		Author: &discordgo.MessageEmbedAuthor{
 			Name:    fmt.Sprintf("%s help", common.Bot.Username),
 			IconURL: common.Bot.AvatarURL("256"),
 		},
-		Description: "Hi there, I'm ASBWIG. As of right now I am vastly unpopulated.\nShould probably add somewhere for you to view all commands huh..",
+		Description: "Here are the available categories and commands:",
 		Color:       common.SuccessGreen,
 	}
-	message := &discordgo.MessageSend{
-		Embed: basicEmbed,
+	for _, categoryName := range categoryNames {
+		// Sort commands within the category
+		sort.Strings(categories[categoryName])
+		categoryStr := fmt.Sprintf("**%s**: `%s`", categoryName, strings.Join(categories[categoryName], "`, `"))
+		helpEmbed.Description += "\n\n" + categoryStr
 	}
-	functions.SendMessage(data.ChannelID, message)
+
+	message := &discordgo.MessageSend{
+		Embed: helpEmbed,
+	}
+	functions.SendMessage(channelID, message)
 }
 
 func help(command string, channelID string) {
