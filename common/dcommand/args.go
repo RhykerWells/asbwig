d043c4ae21e617130987938f85580810f4257259
Rhyker Wells <a.rhykerw@gmail.com>
2025-04-22 19:10:48 +0000

    feat(common/dcommand): Add new argument types

--- common/dcommand/args.go
+++ common/dcommand/args.go
@@ -16,6 +16,9 @@ var (
 	Int    = &IntArg{}
 	User   = &UserArg{}
 	Channel = &ChannelArg{}
+	Bet = &BetArg{}
+	CoinSide = &CoinSideArg{}
+	UserBalance = &BalanceArg{}
 )
 
 type AnyArg struct{}
@@ -46,4 +49,28 @@ type ChannelArg struct{}
 var _ ArgumentType = (*ChannelArg)(nil)
 func (c *ChannelArg) Help() string {
 	return "Mention/ID"
+}
+
+type BetArg struct{}
+var _ ArgumentType = (*BetArg)(nil)
+func (b *BetArg) Help() string {
+	return "Whole integer|max|all"
+}
+
+type MoneyArg struct{}
+var _ ArgumentType = (*MoneyArg)(nil)
+func (m *MoneyArg) Help() string {
+	return "Whole integer above 0"
+}
+
+type CoinSideArg struct{}
+var _ ArgumentType = (*CoinSideArg)(nil)
+func (c *CoinSideArg) Help() string {
+	return "Heads/Tails"
+}
+
+type BalanceArg struct{}
+var _ ArgumentType = (*BalanceArg)(nil)
+func (b *BalanceArg) Help() string {
+	return "Bank/Cash"
 }
