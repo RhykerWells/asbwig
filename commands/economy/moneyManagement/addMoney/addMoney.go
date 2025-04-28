6fa535c35b981019e165a3dfc58c45ed28fa21ea
Rhyker Wells <a.rhykerw@gmail.com>
2025-04-28 12:20:29 +0000

    feat(economy/moneyManagement): Switch to new arg handling

--- commands/economy/moneyManagement/addMoney/addMoney.go
+++ commands/economy/moneyManagement/addMoney/addMoney.go
@@ -22,45 +22,16 @@ var Command = &dcommand.AsbwigCommand{
 	Description: "Adds money to a specified users cash/bank balance",
 	Args: []*dcommand.Args{
 		{Name: "User", Type: dcommand.User},
-		{Name: "Place", Type: dcommand.String},
+		{Name: "Place", Type: dcommand.UserBalance},
 		{Name: "Amount", Type: dcommand.Int},
 	},
+	ArgsRequired: 3,
 	Run: util.AdminOrManageServerCommand(func(data *dcommand.Data) {
 		embed := &discordgo.MessageEmbed{Author: &discordgo.MessageEmbedAuthor{Name: data.Author.Username, IconURL: data.Author.AvatarURL("256")}, Timestamp: time.Now().Format(time.RFC3339), Color: common.ErrorRed}
 		guild, _ := models.EconomyConfigs(qm.Where("guild_id=?", data.GuildID)).One(context.Background(), common.PQ)
-		if len(data.Args) <= 0 {
-			embed.Description = "No `User` argument provided"
-			functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
-			return
-		}
-		member, err := functions.GetMember(data.GuildID, data.Args[0])
-		if err != nil {
-			embed.Description = "Invalid `User` argument provided"
-			functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
-			return
-		}
-		if len(data.Args) <= 1 {
-			embed.Description = "No `Destination` argument provided"
-			functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
-			return
-		}
+		member, _ := functions.GetMember(data.GuildID, data.Args[0])
 		destination := data.Args[1]
-		if destination != "cash" && destination != "bank" {
-			embed.Description = "Invalid `Destination` argument provided\nPlease use `cash` or `bank`"
-			functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
-			return
-		}
-		if len(data.Args) <= 2 {
-			embed.Description = "No `Amount` argument provided"
-			functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
-			return
-		}
 		amount := data.Args[2]
-		if functions.ToInt64(amount) <= 0 {
-			embed.Description = "Invalid `Amount` argument provided"
-			functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
-			return
-		}
 		economyUser, err := models.EconomyUsers(qm.Where("guild_id=? AND user_id=?", data.GuildID, member.User.ID)).One(context.Background(), common.PQ)
 		var cash, bank int64
 		if err == nil {
