23c858e6e4721e8ce90b497b6e3e01a0279521fc
Rhyker Wells <a.rhykerw@gmail.com>
2025-05-11 17:27:45 +0000

    feat(bot): Add ChangeGuildPrefix function

--- bot/prefix/prefix.go
+++ bot/prefix/prefix.go
@@ -22,3 +22,8 @@ func addDefaultPrefix(guild string) {
 	const query = `INSERT INTO core_config (guild_id, guild_prefix) VALUES ($1, '~')`
 	common.PQ.Exec(query, guild)
 }
+
+func ChangeGuildPrefix(guildID, prefix string) {
+	const query = `UPDATE core_config SET guild_prefix = $1 WHERE guild_id = $2`
+	common.PQ.Exec(query, prefix, guildID)
+}
