9ba8a0a8f883c7dc8852975722f0f47fdd7df21b
Rhyker Wells <a.rhykerw@gmail.com>
2025-05-12 07:46:54 +0000

    feat(util): Add IsGuildBanned

--- commands/util/util.go
+++ commands/util/util.go
@@ -1,10 +1,14 @@
 package util
 
 import (
+	"context"
+
 	"github.com/RhykerWells/asbwig/bot/functions"
 	"github.com/RhykerWells/asbwig/common"
 	"github.com/RhykerWells/asbwig/common/dcommand"
+	"github.com/RhykerWells/asbwig/common/models"
 	"github.com/bwmarrin/discordgo"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 func OwnerCommand(inner dcommand.Run) dcommand.Run {
@@ -26,4 +30,14 @@ func AdminOrManageServerCommand(inner dcommand.Run) dcommand.Run {
 			functions.SendBasicMessage(data.ChannelID, "You need `Administrator` or `ManageServer` permissions to use this command.")
 		}
 	}
+}
+
+// IsGuildBanned returns a boolean of whether the guild is banned or not
+func IsGuildBanned(guildID string) bool {
+	exists, err := models.BannedGuilds(qm.Where("guild_id = ?", guildID)).Exists(context.Background(), common.PQ)
+	if err != nil {
+		return false
+	}
+
+	return exists
 }
