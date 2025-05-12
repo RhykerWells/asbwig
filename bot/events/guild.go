0108568742b0567fcce33106d95b6e082f78bcdc
Rhyker Wells <a.rhykerw@gmail.com>
2025-05-12 07:35:50 +0000

    feat(bot): Prevent bot joining a banned guild

--- bot/events/guild.go
+++ bot/events/guild.go
@@ -1,23 +1,32 @@
 package events
 
 import (
+	"context"
+
 	"github.com/RhykerWells/asbwig/bot/prefix"
 	"github.com/RhykerWells/asbwig/commands/economy"
 	"github.com/RhykerWells/asbwig/common"
+	"github.com/RhykerWells/asbwig/common/models"
 	"github.com/bwmarrin/discordgo"
 	log "github.com/sirupsen/logrus"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 // guildJoin is called when the bot is added to a new guild
 // This adds the guild to the relevant database tables
 func guildJoin(s *discordgo.Session, g *discordgo.GuildCreate) {
-	prefix.GuildPrefix(g.ID)
-	economy.GuildEconomyAdd(g.ID)
 	log.WithFields(log.Fields{
 		"guild":       g.ID,
 		"owner":       g.OwnerID,
 		"membercount": g.MemberCount,
 	}).Infoln("Joined guild: ", g.Name)
+	banned := isGuildBanned(g.ID)
+	if banned {
+		common.Session.GuildLeave(g.ID)
+		return
+	}
+	prefix.GuildPrefix(g.ID)
+	economy.GuildEconomyAdd(g.ID)
 }
 
 // guildLeave is called when the bot is removed from a guild
@@ -30,9 +39,16 @@ func guildLeave(s *discordgo.Session, g *discordgo.GuildDelete) {
 	log.Infoln("Left guild: ", g.ID)
 }
 
-func removeGuildConfig(guild string) {
-	const query = `
-    DELETE FROM core_config WHERE guild_id=$1
-    `
-	common.PQ.Exec(query, guild)
+func removeGuildConfig(guildID string) {
+	const query = `DELETE FROM core_config WHERE guild_id=$1`
+	common.PQ.Exec(query, guildID)
+}
+
+func isGuildBanned(guildID string) bool {
+	exists, err := models.BannedGuilds(qm.Where("guild_id = ?", guildID)).Exists(context.Background(), common.PQ)
+	if err != nil {
+		return false
+	}
+
+	return exists
 }
