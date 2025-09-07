e332fa045effcf9b60a85c8d7c81d0a211798c9a
Rhyker Wells <a.rhykerw@gmail.com>
2025-09-07 18:22:46 +0000

    feat(web): move context data to separate file

--- web/middleware.go
+++ web/middleware.go
@@ -7,8 +7,6 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
-	"sort"
-	"net/url"
 	"strconv"
 	"strings"
 	"time"
@@ -165,70 +163,6 @@ func isUserManaged(guildID string, member *discordgo.Member) bool {
 	return false
 }
 
-// dashboardContextData returns all the necessary context data that we use within the site into one data map
-func dashboardContextData(w http.ResponseWriter, r *http.Request) map[string]interface{} {
-	userData, _ := checkCookie(w, r)
-	userID, _ := userData["id"].(string)
-
-	// Set the list of guilds that the user manages
-	guilds := getUserManagedGuilds(userID)
-	guildList := make([]map[string]interface{}, 0)
-	for guildID, guildName := range guilds {
-		avatarURL := URL + "/static/img/icons/cross.png"
-		if guild, err := common.Session.Guild(guildID); err == nil {
-			if url := guild.IconURL("1024"); url != "" {
-				avatarURL = url
-			}
-		}
-		guildList = append(guildList, map[string]interface{}{
-			"ID":   guildID,
-			"Avatar": avatarURL,
-			"Name": guildName,
-		})
-	}
-
-	// If a guild is selected, populate a map of data
-	// TODO: host goji internally and modify pat.Param to return blank string instead of panic when param is not found.
-	var guildID string
-	if strings.Contains(r.URL.Path, "/manage") {
-		guildID = pat.Param(r, "server")
-	}
-	guildData := getGuildData(guildID)
-
-	urls := getUrlData()
-
-	// Marshal the guild data into JSON and write to the response
-	responseData := map[string]interface{}{
-		"User": userData,
-		"ManagedGuilds": guildList,
-		"Year": time.Now().UTC().Year(),
-		"URLs": urls,
-		"CurrentGuild": guildData,
-	}
-	return responseData
-}
-
-func getUrlData() (urlData map[string]interface{}) {
-	u, err := url.Parse(TermsURL)
-	termsURL := URL + "/terms"
-	if err == nil {
-		termsURL = u.String()
-	}
-
-	u, err = url.Parse(PrivacyURL)
-	privacyURL := URL + "/privacy"
-	if err == nil {
-		privacyURL = u.String()
-	}
-	urlData = map[string]interface{}{
-		"Home": URL,
-		"Terms": termsURL,
-		"Privacy": privacyURL,
-	}
-
-	return urlData
-}
-
 // validateGuild ensures users can't access the manage page for guilds without the correct permissions
 func validateGuild(inner http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -260,71 +194,6 @@ func validateGuild(inner http.Handler) http.Handler {
 
 		inner.ServeHTTP(w, r)
 	})
-}
-
-// getGuildData retrieves select data about the guild to use within the manage page of the dashboard
-func getGuildData(guildID string) (guildData map[string]interface{}) {
-	if guildID == "" {
-		return guildData
-	}
-	retrievedGuild, _ := common.Session.Guild(guildID)
-	channels, _ := common.Session.GuildChannels(guildID)
-	roles := retrievedGuild.Roles
-	sort.SliceStable(channels, func(i, j int) bool {
-		return channels[i].Position < channels[j].Position
-	})
-	sort.SliceStable(roles, func(i, j int) bool {
-		return roles[i].Position > roles[j].Position
-	})
-	moderationData := getGuildModerationSettings(guildID)
-	member, _ := functions.GetMember(retrievedGuild.ID, common.Bot.ID)
-	role := functions.HighestRole(retrievedGuild.ID, member)
-	guildData = map[string]interface{}{
-		"ID": retrievedGuild.ID,
-		"Name": retrievedGuild.Name,
-		"Avatar": retrievedGuild.IconURL("1024"),
-		"Channels": channels,
-		"Roles": roles,
-		"ModerationConfig": moderationData,
-		"BotHighestRolePosition": role.Position,
-	}
-	if guildData["Avatar"] == "" {
-		guildData["Avatar"] = URL + "/static/img/icons/cross.png"
-	}
-	return guildData
-}
-
-func getGuildModerationSettings(guildID string) map[string]interface{} {
-	config, _ := models.ModerationConfigs(qm.Where("guild_id=?", guildID)).One(context.Background(), common.PQ)
-	commandRestrictions := getRoleRestrictions(guildID)
-	triggerSettings := map[string]interface{}{
-		"Enabled": config.EnabledTriggerDeletion,
-		"Seconds": config.SecondsToDeleteTrigger,
-	}
-	responseSettings := map[string]interface{}{
-		"Enabled": config.EnabledResponseDeletion,
-		"Seconds": config.SecondsToDeleteResponse,
-	}
-	moderationSettings := map[string]interface{} {
-		"Enabled": config.Enabled,
-		"Modlog": config.ModLog.String,
-		"Restrictions": commandRestrictions,
-		"TriggerSettings": triggerSettings,
-		"ResponseSettings": responseSettings,
-		"MuteRole": config.MuteRole.String,
-		"ManageMuteRole": config.ManageMuteRole,
-		"MuteUpdateRole": config.UpdateRoles,
-	}
-	return moderationSettings
-}
-
-func getRoleRestrictions(guildID string) map[string][]string {
-	roles, _ := models.ModerationConfigRoles(qm.Where("guild_id = ?", guildID)).All(context.Background(), common.PQ)
-	commandRestrictions := make(map[string][]string)
-	for _, role := range roles {
-		commandRestrictions[role.ActionType] = append(commandRestrictions[role.ActionType], role.RoleID)
-	}
-	return commandRestrictions
 }
 
 // handleUpdatePrefix changes the guilds prefix in the database with the one provided from the dashboard
