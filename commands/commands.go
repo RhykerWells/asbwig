81769835ed0cb1cd017ada874a8e17d0c5ab2bb7
Rhyker Wells <a.rhykerw@gmail.com>
2025-09-19 10:54:47 +0000

    feat(commands): add moderation to command handler

--- commands/commands.go
+++ commands/commands.go
@@ -5,6 +5,7 @@ import (
 	"github.com/bwmarrin/discordgo"
 
 	"github.com/RhykerWells/asbwig/commands/economy"
+	"github.com/RhykerWells/asbwig/commands/moderation"
 	"github.com/RhykerWells/asbwig/commands/invite"
 	"github.com/RhykerWells/asbwig/commands/ping"
 
@@ -35,5 +36,6 @@ func InitCommands(session *discordgo.Session) {
 	)
 
 	economy.EconomySetup(cmdHandler)
+	moderation.ModerationSetup(cmdHandler)
 	session.AddHandler(cmdHandler.HandleMessageCreate)
 }
