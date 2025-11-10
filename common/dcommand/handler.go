a43dbbeeb9cff610d08f5f9d4be03bd5331c0d87
Rhyker Wells <a.rhykerw@gmail.com>
2025-11-10 01:04:48 +0000

    feat(dcommand): restructure argument handling and pass argument values to parsedArg

--- common/dcommand/handler.go
+++ common/dcommand/handler.go
@@ -1,14 +1,10 @@
 package dcommand
 
 import (
-	"fmt"
-	"reflect"
 	"strings"
-	"time"
 
 	"github.com/RhykerWells/summit/bot/functions"
 	prfx "github.com/RhykerWells/summit/bot/prefix"
-	"github.com/RhykerWells/summit/common"
 	"github.com/bwmarrin/discordgo"
 	"github.com/sirupsen/logrus"
 )
@@ -42,11 +38,6 @@ func (c *CommandHandler) HandleMessageCreate(s *discordgo.Session, event *discor
 	}
 
 	command := strings.ToLower(prefixRemoved[0])
-	argsNotLowered := prefixRemoved[1:]
-	var args []string
-	for _, arg := range argsNotLowered {
-		args = append(args, strings.ToLower(arg))
-	}
 
 outer:
 	for _, cmd := range c.cmdMap {
@@ -62,18 +53,19 @@ outer:
 		return
 	}
 
+	commandArgs := prefixRemoved[1:]
+
 	data := &Data{
-		Session:        s,
-		GuildID:        event.GuildID,
-		ChannelID:      event.ChannelID,
-		Author:         event.Author,
-		Args:           args,
-		ArgsNotLowered: argsNotLowered,
-		Handler:        c,
-		Message:        event.Message,
+		Session:    s,
+		GuildID:    event.GuildID,
+		ChannelID:  event.ChannelID,
+		Author:     event.Author,
+		ParsedArgs: nil,
+		Handler:    c,
+		Message:    event.Message,
 	}
 
-	go runCommand(cmd, data)
+	go runCommand(cmd, data, commandArgs)
 }
 
 // checkMessagePrefix checks a given content for the prefix or bot mention of the guild
@@ -114,18 +106,34 @@ func findMentionPrefix(botID string, message string) (string, bool) {
 }
 
 // runCommand logs the command called by the bot, checks the required args, their types and then runs the command
-func runCommand(cmd SummitCommand, data *Data) {
+func runCommand(cmd SummitCommand, data *Data, commandArgs []string) {
 	logrus.WithFields(logrus.Fields{
 		"Guild":           data.GuildID,
 		"Command":         cmd.Command,
 		"Triggering user": data.Author.ID},
 	).Infoln("Executed command")
 
-	argCount := len(data.Args)
+	argCount := len(commandArgs)
 	if argCount < cmd.ArgsRequired {
 		handleMissingArgs(cmd, data)
 		return
 	}
+
+	var parsedArgs []*ParsedArg
+	for i, argValue := range commandArgs {
+		if i == len(cmd.Args)-1 {
+			argValue = strings.Join(commandArgs[i:], " ")
+		}
+
+		arg := cmd.Args[i]
+		parsedArgs = append(parsedArgs, &ParsedArg{
+			Name:  arg.Name,
+			Type:  arg.Type,
+			Value: argValue,
+		})
+	}
+	data.ParsedArgs = parsedArgs
+
 	if argCount > 0 {
 		if embed, invalid := handleInvalidArgs(cmd, data); invalid {
 			functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
@@ -134,83 +142,4 @@ func runCommand(cmd SummitCommand, data *Data) {
 	}
 
 	cmd.Run(data)
-}
-
-// handleMissingArgs sends message notifying that their are required arguments missing
-func handleMissingArgs(cmd SummitCommand, data *Data) {
-	args := cmd.Args
-	var missingArgs []*Args
-	for _, arg := range args {
-		if arg.Optional {
-			continue
-		}
-		missingArgs = append(missingArgs, arg)
-	}
-	var argDisplay string
-	for _, arg := range missingArgs {
-		argDisplay += fmt.Sprintf("<%s:%s> ", arg.Name, arg.Type.Help())
-	}
-	embed := errorEmbed(cmd.Command, data, fmt.Sprintf("Missing required args\n```%s %s```", cmd.Command, argDisplay))
-	functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
-}
-
-// handleInvalidArgs sends a message notifying the user that an argument they have attempted to use is invalid
-func handleInvalidArgs(cmd SummitCommand, data *Data) (*discordgo.MessageEmbed, bool) {
-	for i, arg := range cmd.Args {
-		input := data.Args[i]
-		errorMessage := fmt.Sprintf("Invalid `%s` arg provided.", arg.Name)
-		switch reflect.TypeOf(arg.Type).String() {
-		case "*dcommand.StringArg":
-			return nil, false
-		case "*dcommand.IntArg":
-			if functions.ToInt64(input) <= 0 {
-				return errorEmbed(cmd.Command, data, fmt.Sprintf("%s\nPlease provide a whole number above 0.", errorMessage)), true
-			}
-		case "*dcommand.UserArg":
-			if _, err := functions.GetUser(input); err != nil {
-				return errorEmbed(cmd.Command, data, fmt.Sprintf("%s\nPlease provide a valid user mention or ID.", errorMessage)), true
-			}
-		case "*dcommand.MemberArg":
-			if _, err := functions.GetMember(data.GuildID, input); err != nil {
-				return errorEmbed(cmd.Command, data, fmt.Sprintf("%s\nPlease provide a valid user mention or ID.", errorMessage)), true
-			}
-		case "*dcommand.ChannelArg":
-			if _, err := functions.GetChannel(data.GuildID, input); err != nil {
-				return errorEmbed(cmd.Command, data, fmt.Sprintf("%s\nPlease provide a channel mention or ID.", errorMessage)), true
-			}
-		case "*dcommand.BetArg":
-			if functions.ToInt64(input) <= 0 && input != "all" && input != "max" {
-				return errorEmbed(cmd.Command, data, fmt.Sprintf("%s\nPlease provide a whole number, `all`, or `max`.", errorMessage)), true
-			}
-		case "*dcommand.CoinSideArg":
-			if input != "heads" && input != "tails" {
-				return errorEmbed(cmd.Command, data, fmt.Sprintf("%s\nPlease provide `heads` or `tails`.", errorMessage)), true
-			}
-		case "*dcommand.BalanceArg":
-			if input != "cash" && input != "bank" {
-				return errorEmbed(cmd.Command, data, fmt.Sprintf("%s\nPlease provide `cash` or `bank`.", errorMessage)), true
-			}
-		case "*dcommand.ResponseType":
-			if input != "work" && input != "crime" {
-				return errorEmbed(cmd.Command, data, fmt.Sprintf("%s\nPlease provide `work` or `crime`", errorMessage)), true
-			}
-		default:
-			return errorEmbed(cmd.Command, data, "Something went wrong handling the arguments."), true
-		}
-	}
-	// Return nil if no errors
-	return nil, false
-}
-
-// errorEmbed returns a populated embed object to denote an error in the command execution
-func errorEmbed(cmd string, data *Data, description string) *discordgo.MessageEmbed {
-	return &discordgo.MessageEmbed{
-		Author: &discordgo.MessageEmbedAuthor{
-			Name:    data.Author.Username + " - " + cmd,
-			IconURL: data.Author.AvatarURL("256"),
-		},
-		Timestamp:   time.Now().Format(time.RFC3339),
-		Color:       common.ErrorRed,
-		Description: description,
-	}
 }
---
+++ common/dcommand/parse.go
@@ -0,0 +1,131 @@
+package dcommand
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/RhykerWells/durationutil"
+	"github.com/RhykerWells/summit/bot/functions"
+	"github.com/RhykerWells/summit/common"
+	"github.com/bwmarrin/discordgo"
+)
+
+type ParsedArg struct {
+	Name  string
+	Type  ArgumentType
+	Value interface{}
+}
+
+func (p *ParsedArg) String() string {
+	if p.Value == nil {
+		return ""
+	}
+
+	switch t := p.Value.(type) {
+	case string:
+		return t
+	case int, int32, int64, uint, uint32, uint64:
+		return strconv.FormatInt(functions.ToInt64(t), 10)
+	default:
+		return ""
+	}
+}
+
+func (p *ParsedArg) Int64() int64 {
+	if p.Value == nil {
+		return 0
+	}
+
+	return functions.ToInt64(p.Value)
+}
+
+func (p *ParsedArg) User() *discordgo.User {
+	if p.Value == nil {
+		return nil
+	}
+
+	user, _ := functions.GetUser(p.String())
+	return user
+}
+
+func (p *ParsedArg) Member(guildID string) *discordgo.Member {
+	if p.Value == nil {
+		return nil
+	}
+
+	member, _ := functions.GetMember(guildID, p.String())
+	return member
+}
+
+func (p *ParsedArg) Duration() *time.Duration {
+	if p.Value == nil {
+		return nil
+	}
+
+	duration, _ := durationutil.ToDuration(p.String())
+	return &duration
+}
+
+func (p *ParsedArg) Coin() string {
+	if p.Value == nil {
+		return ""
+	}
+
+	return strings.ToLower(strings.TrimSpace(p.String()))
+}
+
+func (p *ParsedArg) BalanceType() string {
+	if p.Value == nil {
+		return ""
+	}
+
+	return strings.ToLower(strings.TrimSpace(p.String()))
+}
+
+// handleMissingArgs sends message notifying that their are required arguments missing
+func handleMissingArgs(cmd SummitCommand, data *Data) {
+	args := cmd.Args
+	var missingArgs []*Args
+
+	for _, arg := range args {
+		if arg.Optional {
+			continue
+		}
+		missingArgs = append(missingArgs, arg)
+	}
+
+	var argDisplay string
+	for _, arg := range missingArgs {
+		argDisplay += fmt.Sprintf("<%s:%s> ", arg.Name, arg.Type.Help())
+	}
+
+	embed := errorEmbed(cmd.Command, data, fmt.Sprintf("Missing required args\n```%s %s```", cmd.Command, argDisplay))
+	functions.SendMessage(data.ChannelID, &discordgo.MessageSend{Embed: embed})
+}
+
+// handleInvalidArgs sends a message notifying the user that an argument they have attempted to use is invalid
+func handleInvalidArgs(cmd SummitCommand, data *Data) (*discordgo.MessageEmbed, bool) {
+	for _, arg := range data.ParsedArgs {
+		if !arg.Type.ValidateArg(arg, data) {
+			return errorEmbed(cmd.Command, data, fmt.Sprintf("Invalid `%s` argument. Expected: `%s`", cmd.Command, arg.Type.Help())), true
+		}
+	}
+
+	// Return nil if no errors
+	return nil, false
+}
+
+// errorEmbed returns a populated embed object to denote an error in the command execution
+func errorEmbed(cmd string, data *Data, description string) *discordgo.MessageEmbed {
+	return &discordgo.MessageEmbed{
+		Author: &discordgo.MessageEmbedAuthor{
+			Name:    data.Author.Username + " - " + cmd,
+			IconURL: data.Author.AvatarURL("256"),
+		},
+		Timestamp:   time.Now().Format(time.RFC3339),
+		Color:       common.ErrorRed,
+		Description: description,
+	}
+}
